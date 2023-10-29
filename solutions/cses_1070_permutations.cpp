804f84e10037550681efc67c1a635c6e0610e33f
Sam Mercer <smercer10@proton.me>
2023-10-29 19:19:36 +0000

    Add cses_1070_permutations

---
+++ solutions/cses_1070_permutations.cpp
@@ -0,0 +1,73 @@
+/*                                                ____________
+______________ ___________________________________<  /_  __ \
+__  ___/_  __ `__ \  _ \_  ___/  ___/  _ \_  ___/_  /_  / / /
+_(__  )_  / / / / /  __/  /   / /__ /  __/  /   _  / / /_/ /
+/____/ /_/ /_/ /_/\___//_/    \___/ \___//_/    /_/  \____/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthands
+using LL = long long;
+using VI = vector<int>;
+using VLL = vector<LL>;
+using VVI = vector<VI>;
+using VVLL = vector<VLL>;
+using PI = pair<int, int>;
+using PLL = pair<LL, LL>;
+#define NL '\n'
+#define PB push_back
+#define MP make_pair
+#define F(i, b, e) for (LL i = b; i < e; i++)
+#define FI(i, b, e) for (LL i = b; i <= e; i++)
+#define FR(i, e, b) for (LL i = e - 1; i >= b; i--)
+#define FE(e, c) for (auto& e : c)
+
+// Utilities
+LL min(int a, LL b) { return a < b ? a : b; }
+LL min(LL a, int b) { return a < b ? a : b; }
+LL max(int a, LL b) { return a > b ? a : b; }
+LL max(LL a, int b) { return a > b ? a : b; }
+
+void solve();
+
+// Testing
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+
+  solve();
+  cerr << "Runtime: " << (double)clock() / CLOCKS_PER_SEC << "s";
+}
+
+// Solution
+void solve() {
+  int n;
+  cin >> n;
+
+  if (n == 2 || n == 3) {
+    cout << "NO SOLUTION";
+    return;
+  }
+
+  int e = 2;
+
+  while (e <= n) {
+    cout << e << ' ';
+    e += 2;
+  }
+
+  e = 1;
+
+  while (e <= n) {
+    cout << e << ' ';
+    e += 2;
+  }
+}
